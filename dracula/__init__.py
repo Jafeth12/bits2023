18c8ea5169a00ff85566e14d186c1e3f8637cbff
jafeth <jafethrafael12@gmail.com>
2023-12-16 12:58:03 +0100

    init

---
+++ dracula/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template as template
+from flask_assets import Bundle, Environment
+
+app = Flask(__name__)
+
+assets = Environment(app)
+css = Bundle('src/main.css', output='dist/main.css')
+js = Bundle("src/*.js", output="dist/main.js")
+
+assets.register('css', css)
+assets.register("js", js)
+css.build()
+js.build()
+
+@app.route('/')
+def index():
+    return template('index.html')
