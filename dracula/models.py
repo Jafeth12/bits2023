292ff01be3005f549af2218ec93f59b963f5f1c5
SW1073 <eloiimerino@gmail.com>
2023-12-17 02:54:40 +0100

    Puntuacion por ciclo y añadir nuevos ciclos en página principal

--- dracula/models.py
+++ dracula/models.py
@@ -8,16 +8,22 @@ class Sample(db.Model):
     score: Mapped[int] = mapped_column(db.Integer, nullable=False)
     day_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('day.id'), nullable=False)
 
+    def __init__(self, filename, score, day_id):
+        self.filename = filename
+        self.score = score
+        self.day_id = day_id
+
 class Day(db.Model):
     id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
     date: Mapped[str] = mapped_column(db.String(80), unique=True, nullable=False)
     cicle_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('cicle.id'), nullable=False)
 
+    def __init__(self, date, cicle_id):
+        self.date = date
+        self.cicle_id = cicle_id
+
 class Cicle(db.Model):
     id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-
-    def __init__(self, id):
-        self.id = id
 
 with app.app_context():
     db.create_all()
--- dracula/routes.py
+++ dracula/routes.py
@@ -1,15 +1,20 @@
 import os
 from flask import render_template, request, jsonify, url_for
 from dracula import app, db
-from dracula.models import Cicle
+from dracula.models import Cicle, Day, Sample
 
 @app.route('/')
 def index():
-    return render_template('home.html', cicles=Cicle.query.all())
+    return home()
 
 @app.route('/home')
 def home():
-    return render_template('home.html', cicles=Cicle.query.all())
+    cicles = Cicle.query.all()
+    punts = []
+    for cicle in cicles:
+        punts.append(get_cicle_score(cicle))
+    cicles_with_scores = zip(cicles, punts)
+    return render_template('home.html', cicles_with_scores=cicles_with_scores)
 
 @app.route('/uploads')
 def uploads():
@@ -31,3 +36,29 @@ def upload_image():
         filename = file.filename or ''
         file.save(os.path.join('dracula/static/upload', filename))
         return '<p>File recieved</p>'
+
+@app.route('/newcicle', methods=['POST'])
+def cicle():
+    cicle = Cicle()
+    db.session.add(cicle)
+    db.session.commit()
+    return 'Afegir cicle';
+
+# @app.route('/dbg', methods=['GET'])
+# def dbg():
+#     sample = Sample('sample_xd', 88, 1)
+#     db.session.add(sample)
+#     db.session.commit()
+#     return 'a'
+
+# Hacer el sumatorio de los scores de los samples de un ciclo
+def get_cicle_score(cicle):
+    days = Day.query.filter_by(cicle_id=cicle.id)
+    total = 0
+    for day in days:
+        samples = Sample.query.filter_by(day_id=day.id)
+        for sample in samples:
+            total += sample.score
+    return total
+
+
